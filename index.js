111014b324542e953b637eeffadc6f895305a045
Denny06 <denismacio@outlook.com>
2020-07-02 16:00:20 +0300

    added start script

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const upload = multer();
 
 const cookieParser = require('cookie-parser');
 
-require('dotenv').config();
+//require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const SU_PASS = process.env.SU_PASS;
 const SESSION_SECRET = process.env.SESSION_SECRET;
